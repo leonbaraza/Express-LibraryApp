5d43e68c9259e1ce329c7c7daff833576b1576e6
Leon <leonbaraza@gmail.com>
2020-06-26 10:44:49 +0300

    API - Complete Book routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -36,11 +36,78 @@ router.get('/:id', async (req, res) => {
         book = await Book.findById(req.params.id)
                         .populate('author')
                         .exec()
+        if(book == null){
+            res.status(404).json({message: `Cannot find book with id of ${req.params.id}`})
+        }
         res.json(book)
     } catch (err) {
         res.status(400).json({ Error: `Book of id ${req.params.id} not found. Ensure you gave the correct id`})
     }
 })
 
+// Add a new book
+router.post('/', async (req, res) => {
+    let book  = new Book({
+        title: req.body.title.trim(),
+        author: req.body.author,
+        publishDate: new Date(req.body.publish_date),
+        pageCount: req.body.page_count,
+        description: req.body.description.trim()
+    })
+    saveCover(book, req.body.cover)
+    try {
+        await book.save()
+        res.json(book)
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
+
+// Update an existing book
+router.put('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        book.title  = req.body.title.trim(),
+        book.author = req.body.author,
+        book.publishDate = new Date(req.body.publish_date),
+        book.pageCount = req.body.page_count,
+        book.description = req.body.description.trim()
+
+        if (book.cover != null && book.cover !== '') {
+            saveCover(book, req.body.cover)            
+        }
+
+        await book.save()
+        res.json(book)
+    } catch (err) {
+        res.status(400).json({ Error: err.message})
+    }
+})
+
+
+router.delete('/:id', getBook, async(req, res) => {
+    try {
+        await res.book.remove()
+        res.json({ message: 'Book was successfully removed' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+async function getBook(req, res, next){
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        if(book == null){
+            res.status(404).json({message: `Cannot find book with id of ${req.params.id}`})
+        }
+    } catch (err) {
+        res.status(500).json({ message:err.message })
+    }
+    res.book = book
+    next()
+}
 
 module.exports = router
